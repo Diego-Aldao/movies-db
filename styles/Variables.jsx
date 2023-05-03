c183b343880ae2493943d67a8cf02436fd7255d1
Diego Aldao <diego_ind@live.com.ar>
2023-05-03 20:24:59 -0300

    añadidas variables para colores y textos utilizando css de styled-components

---
+++ styles/Variables.jsx
@@ -0,0 +1,16 @@
+import { css } from "styled-components";
+
+const Variables = css`
+  :root {
+    --color-principal: #7effb7;
+    --bg-principal: #0b0c17;
+    --bg-secundario: #191a2b;
+    --color-texto-principal: #ece6f7;
+    --color-texto-secundario: #bbb8c5;
+    --color-texto-terciario: #a6a1b8;
+
+    --fuente-principal: "Raleway", sans-serif;
+  }
+`;
+
+export default Variables;
