efc791bf7262c3c85df06ead974ba33a0af6f89e
Diego Aldao <diego_ind@live.com.ar>
2023-05-04 20:35:03 -0300

    añadido un componente que utiliza react-select y devuelve la opcion seleccionada por el usuario para usarla como query en el filtro del fetch

---
+++ src/components/PaginaPrincipal/Tendencias.jsx/Filtro.jsx
@@ -0,0 +1,50 @@
+import Select from "react-select";
+const styles = {
+  control: (styles, state) => ({
+    ...styles,
+    backgroundColor: "var(--bg-secundario)",
+
+    border: state.isFocused ? "red" : "aqua",
+  }),
+  singleValue: (base) => ({
+    ...base,
+    color: "var(--color-texto-principal)",
+  }),
+  menu: (styles) => ({
+    ...styles,
+    backgroundColor: "var(--bg-secundario)",
+  }),
+  option: (styles, state) => ({
+    ...styles,
+    backgroundColor: state.isSelected
+      ? "var(--color-principal)"
+      : "var(--bg-secundario)",
+    color: state.isFocused
+      ? "var(--bg-principal)"
+      : state.isSelected
+      ? "var(--bg-principal)"
+      : "var(--color-texto-principal)",
+    ":hover": {
+      ...styles[":hover"],
+      backgroundColor: "var(--color-principal)",
+    },
+  }),
+};
+
+const Filtro = ({ options, setState }) => {
+  return (
+    <Select
+      className="select"
+      defaultValue={options[0]}
+      name="date"
+      options={options}
+      isSearchable={false}
+      styles={styles}
+      onChange={(e) => {
+        setState(e.value);
+      }}
+    />
+  );
+};
+
+export default Filtro;
