626f50b56cb4310e9454bb2e19b953f0790209ea
Diego Aldao <diego_ind@live.com.ar>
2023-05-04 20:39:01 -0300

    añadido un color secundario y un gradiente

--- styles/Variables.jsx
+++ styles/Variables.jsx
@@ -3,8 +3,10 @@ import { css } from "styled-components";
 const Variables = css`
   :root {
     --color-principal: #7effb7;
+    --color-secundario: #b2fefa;
+    --gradiente: linear-gradient(to right, #7effb7, #b2fefa);
     --bg-principal: #0b0c17;
-    --bg-secundario: #191a2b;
+    --bg-secundario: #333446;
     --color-texto-principal: #ece6f7;
     --color-texto-secundario: #bbb8c5;
     --color-texto-terciario: #a6a1b8;
