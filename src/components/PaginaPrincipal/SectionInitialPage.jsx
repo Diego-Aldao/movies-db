18b53c5be04ec1072640916e37d4238569fc76b8
Diego Aldao <diego_ind@live.com.ar>
2023-05-05 20:51:12 -0300

    creado un componente reutilizable para las secciones de la pagina principal

---
+++ src/components/PaginaPrincipal/SectionInitialPage.jsx
@@ -0,0 +1,154 @@
+import styled, { css } from "styled-components";
+import { Swiper } from "swiper/react";
+import { FreeMode, Scrollbar } from "swiper";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/free-mode";
+
+export const StyledSection = styled.section`
+  width: 100%;
+  ${({ imagen }) =>
+    imagen &&
+    css`
+      background: url(${imagen});
+      background-position: 50% 250px;
+      background-repeat: no-repeat;
+      background-size: contain;
+    `}
+  max-width: 1600px;
+  overflow: hidden;
+  padding-inline: 10px;
+  margin: 100px auto 0px;
+  ${({ banner }) =>
+    banner &&
+    css`
+      position: relative;
+      background: url(${banner});
+      background-position: 50% 0%;
+      background-size: cover;
+      transition: background 0.3s ease-in-out;
+      &:after {
+        background: var(--bg-principal);
+        content: "";
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        filter: opacity(75%);
+      }
+    `}
+  @media (min-width: 768px) {
+    padding-inline: 24px;
+  }
+  @media (min-width: 1024px) {
+    padding-inline: 50px;
+  }
+`;
+
+export const StyledHeader = styled.header`
+  padding-top: 20px;
+  width: 100%;
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
+  position: relative;
+  z-index: 5;
+  margin-bottom: 25px;
+  .select {
+    flex: 1 1 calc(50% - 15px);
+    max-width: 200px;
+    z-index: 22;
+    font-weight: 600;
+    text-transform: capitalize;
+  }
+  h2 {
+    width: 100%;
+  }
+  h2 {
+    text-transform: capitalize;
+  }
+`;
+
+const StyledSwiper = styled(Swiper)`
+  position: relative;
+  overflow: visible;
+  padding-bottom: 40px;
+  .swiper-scroll {
+    cursor: pointer;
+    background: var(--color-texto-terciario);
+    div {
+      background: var(--color-principal);
+    }
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0px;
+    right: -10px;
+    width: 90px;
+    height: 95%;
+    background: linear-gradient(to right, #ffffff00, var(--bg-principal));
+    z-index: 2;
+  }
+  @media (min-width: 768px) {
+    &:after {
+      right: -24px;
+    }
+  }
+  @media (min-width: 1024px) {
+    &:after {
+      right: -50px;
+    }
+  }
+`;
+
+const SectionInitialPage = ({
+  children,
+  currentBreakpoints,
+  titulo,
+  filtros,
+  imagen,
+  banner,
+}) => {
+  return (
+    <StyledSection imagen={imagen} banner={banner}>
+      <StyledHeader>
+        <h2>{titulo}</h2>
+        {filtros}
+      </StyledHeader>
+      <StyledSwiper
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        scrollbar={{
+          hide: false,
+          horizontalClass: "swiper-scroll",
+          draggable: true,
+        }}
+        modules={[FreeMode, Scrollbar]}
+        freeMode={true}
+        slidesPerView={1.85}
+        spaceBetween={15}
+        breakpoints={{
+          480: {
+            slidesPerView: 3.85,
+          },
+          768: {
+            slidesPerView: 4.9,
+            spaceBetween: 25,
+          },
+          1024: {
+            slidesPerView: 6.9,
+          },
+          ...currentBreakpoints,
+        }}
+      >
+        {children}
+      </StyledSwiper>
+    </StyledSection>
+  );
+};
+
+export default SectionInitialPage;
