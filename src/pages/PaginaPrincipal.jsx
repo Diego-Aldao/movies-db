c94857d5c1bd15cd0dafe804b65888f2cc04d2f4
Diego Aldao <diego_ind@live.com.ar>
2023-05-07 20:40:56 -0300

    añadidos los componentes restantes y el layout principal

--- src/pages/PaginaPrincipal.jsx
+++ src/pages/PaginaPrincipal.jsx
@@ -1,7 +1,20 @@
 import Header from "../components/PaginaPrincipal/Header/Header";
+import Tendencias from "../components/PaginaPrincipal/Tendencias/Tendencias";
+import Estrenos from "../components/PaginaPrincipal/Estrenos/Estrenos";
+import TopMovies from "../components/PaginaPrincipal/TopMovies/TopMovies";
+import TopSeries from "../components/PaginaPrincipal/TopSeries/TopSeries";
+import LayoutPrincipal from "../Layout/LayoutPrincipal";
 
 const PaginaPrincipal = () => {
-  return <Header />;
+  return (
+    <LayoutPrincipal>
+      <Header />
+      <Tendencias />
+      <Estrenos />
+      <TopMovies />
+      <TopSeries />
+    </LayoutPrincipal>
+  );
 };
 
 export default PaginaPrincipal;
