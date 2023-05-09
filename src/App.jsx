e1ebf7ffa2b78dec009a5e4d45486dce7bf6df34
Diego Aldao <diego_ind@live.com.ar>
2023-05-08 21:10:15 -0300

    añadidas las rutas para la pagina principal y las paginas de listas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import EstilosGlobales from "../styles/EstilosGlobales";
+import PaginaListas from "./pages/PaginaListas";
 import PaginaPrincipal from "./pages/PaginaPrincipal";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PaginaPrincipal />,
+  },
+  {
+    path: "/peliculas/:lista",
+    element: <PaginaListas />,
+  },
+  {
+    path: "/programas-de-television/:lista",
+    element: <PaginaListas />,
+  },
+  {
+    path: "/celebridades/:lista",
+    element: <PaginaListas />,
+  },
+]);
 
 function App() {
   return (
     <>
       <EstilosGlobales />
-      <PaginaPrincipal />
+      <RouterProvider router={router} />
     </>
   );
 }
