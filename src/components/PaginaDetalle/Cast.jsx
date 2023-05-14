03d3590e68d3b033a82b1c9b11703e90b379b99a
Diego Aldao <diego_ind@live.com.ar>
2023-05-14 19:37:19 -0300

    añadido un componente que muestra el reparto principal con imagen, nombre del actor y personaje que interpreta

---
+++ src/components/PaginaDetalle/Cast.jsx
@@ -0,0 +1,89 @@
+import SectionInitialPage from "../PaginaPrincipal/SectionInitialPage";
+import { SwiperSlide } from "swiper/react";
+import styled from "styled-components";
+
+const StyledSection = styled(SectionInitialPage)`
+  margin-top: 20px;
+  padding: 0px 0px 40px;
+  .swiper {
+    height: auto !important; //la unica manera de sobreescribir esta propiedad del swiper
+  }
+  div:after {
+    display: none;
+  }
+`;
+
+const ItemCast = styled.div`
+  background: var(--bg-secundario);
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  .imagen-persona {
+    height: 40vw;
+    max-height: 150px;
+  }
+  .info-persona {
+    height: 25vw;
+    max-height: 85px;
+  }
+  p {
+    padding-block: 5px;
+  }
+  .nombre {
+    font-size: 14px;
+    color: var(--color-texto-principal);
+    margin-top: 5px;
+  }
+  p:last-child {
+    font-size: 12px;
+  }
+`;
+
+const breakpoints = {
+  200: {
+    slidesPerView: 2.4,
+  },
+  480: {
+    slidesPerView: 3.4,
+  },
+  768: {
+    slidesPerView: 4.4,
+    spaceBetween: 25,
+  },
+  1024: {
+    slidesPerView: 4.4,
+  },
+
+  1240: {
+    slidesPerView: 5.8,
+  },
+};
+
+const Cast = ({ cast }) => {
+  const mainCast = cast.cast.slice(0, 8);
+  return (
+    <StyledSection
+      titulo={"reparto principal"}
+      currentBreakpoints={breakpoints}
+    >
+      {mainCast.map((persona) => (
+        <SwiperSlide key={persona.id}>
+          <ItemCast>
+            <div className="imagen-persona">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${persona.profile_path}`}
+                alt=""
+              />
+            </div>
+            <div className="info-persona">
+              <p className="nombre">{persona.name}</p>
+              <p>{persona.character}</p>
+            </div>
+          </ItemCast>
+        </SwiperSlide>
+      ))}
+    </StyledSection>
+  );
+};
+
+export default Cast;
