74c3d69729e8a0a0d3c566c2b5e90f71a8704087
Diego Aldao <diego_ind@live.com.ar>
2023-05-15 21:49:46 -0300

    añadido el componente que muestra las imagenes tanto de los posters como de los wallpapers en el componente Media

---
+++ src/components/PaginaDetalle/Media/Imagenes.jsx
@@ -0,0 +1,16 @@
+const Imagenes = ({ data }) => {
+  const { file_path } = data;
+  return (
+    <div className="contenedor-img">
+      <picture>
+        <source
+          srcSet={`https://image.tmdb.org/t/p/w200${file_path}`}
+          media="(max-width: 580px)"
+        />
+        <img src={`https://image.tmdb.org/t/p/w500${file_path}`} alt="" />
+      </picture>
+    </div>
+  );
+};
+
+export default Imagenes;
