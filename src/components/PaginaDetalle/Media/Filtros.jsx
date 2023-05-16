64f7caffb3aeefe65178be2bbf656b02e72e000b
Diego Aldao <diego_ind@live.com.ar>
2023-05-15 21:51:50 -0300

    añadido el componente Filtros para la seccion Media de la pagina detalles, que brinda la funcionalidad de cambiar lo que muestra el slider entre wallpapers, posters y videos

---
+++ src/components/PaginaDetalle/Media/Filtros.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const StyledFiltros = styled.ul`
+  display: flex;
+  gap: 20px;
+  li {
+    color: var(--color-texto-principal);
+    text-transform: capitalize;
+  }
+  span {
+    text-transform: capitalize;
+    font-size: 14px;
+  }
+`;
+
+const Filtros = ({ filtros, setCurrentMedia }) => {
+  const handleClick = (item) => {
+    setCurrentMedia(item);
+  };
+
+  return (
+    <StyledFiltros>
+      {filtros.map((item, index) => (
+        <li
+          key={index}
+          onClick={() => {
+            handleClick(item);
+          }}
+        >
+          {item.nombre} <span>{item.data.length}</span>
+        </li>
+      ))}
+    </StyledFiltros>
+  );
+};
+
+export default Filtros;
