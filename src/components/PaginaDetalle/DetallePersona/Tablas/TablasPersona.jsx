dcda1ebb1c2a534bda17bd633c8c50f0c1703128
Diego Aldao <diego_ind@live.com.ar>
2023-05-17 13:37:11 -0300

    añadido un componente que contiene las tablas con la informacion de las participaciones de una celebridad

---
+++ src/components/PaginaDetalle/DetallePersona/Tablas/TablasPersona.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import TablaCast from "./TablaCast";
+import TablasCrew from "./TablasCrew";
+
+const StyledSection = styled.section`
+  width: 100%;
+  header {
+    padding-block: 20px;
+    h2 {
+      text-transform: capitalize;
+    }
+  }
+  table {
+    width: 100%;
+  }
+  td {
+    padding-block: 10px;
+    border-bottom: 2px solid var(--bg-secundario);
+    text-align: center;
+  }
+  .separador {
+    padding-inline: 10px;
+    color: var(--color-texto-principal);
+  }
+  p,
+  span {
+    font-family: var(--fuente-principal);
+    display: inline;
+    font-size: 16px;
+  }
+  p {
+    color: var(--color-texto-principal);
+    font-weight: 700;
+  }
+  span {
+    font-weight: 300;
+    color: var(--color-texto-terciario);
+  }
+  .personaje {
+    color: var(--color-texto-secundario);
+    font-weight: 500;
+  }
+  .descripcion {
+    display: flex;
+    flex-wrap: wrap;
+    text-align: left;
+  }
+`;
+
+const TablasPersona = ({ data }) => {
+  return (
+    <StyledSection>
+      {data && (
+        <>
+          <TablaCast cast={data.cast} />
+          <TablasCrew crew={data.crew} />
+        </>
+      )}
+    </StyledSection>
+  );
+};
+
+export default TablasPersona;
