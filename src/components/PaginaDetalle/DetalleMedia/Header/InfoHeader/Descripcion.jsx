06f6b2d56fe01a76cd825b7b9be63e896b8acfb4
Diego Aldao <diego_ind@live.com.ar>
2023-05-18 16:50:22 -0300

    corregidos los estilos y agregado un condicional para cuando la pelicula / serie no cuenta con una descripcion

--- src/components/PaginaDetalle/DetalleMedia/Header/InfoHeader/Descripcion.jsx
+++ src/components/PaginaDetalle/DetalleMedia/Header/InfoHeader/Descripcion.jsx
@@ -4,6 +4,7 @@ const StyledDescripcion = styled.div`
   h3 {
     text-transform: capitalize;
     font-size: clamp(20px, 3vw, 24px);
+    margin-bottom: 5px;
   }
   p {
     font-size: 16px;
@@ -16,10 +17,13 @@ const StyledDescripcion = styled.div`
 `;
 
 const Descripcion = ({ descripcion }) => {
+  const descripcionFinal = !descripcion
+    ? "Aun no cuenta con una descripcion disponible en español"
+    : descripcion;
   return (
     <StyledDescripcion>
       <h3>vista general</h3>
-      <p>{descripcion}</p>
+      <p>{descripcionFinal}</p>
     </StyledDescripcion>
   );
 };
