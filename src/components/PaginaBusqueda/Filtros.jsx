316e35288ddc0d2cd53e74631670d608c7a0d174
Diego Aldao <diego_ind@live.com.ar>
2023-05-18 16:57:53 -0300

    añadido el componente que muestra los resultados de la busqueda segun su categoria (peliculas, series, personas)

---
+++ src/components/PaginaBusqueda/Filtros.jsx
@@ -0,0 +1,61 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledFiltros = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+  padding-block: 10px;
+  max-width: 500px;
+  span {
+    flex: 1 1 auto;
+    text-align: center;
+    text-transform: capitalize;
+    color: var(--color-texto-principal);
+  }
+`;
+
+const ItemFiltro = ({ valor, nombre, handleClick, total }) => {
+  return (
+    <span
+      onClick={() => {
+        handleClick(valor);
+      }}
+    >
+      ver {nombre} {total}
+    </span>
+  );
+};
+
+const Filtros = ({ total, query }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (valor) => {
+    navigate(`/busqueda/${valor}/${query}`);
+  };
+
+  return (
+    <StyledFiltros>
+      <ItemFiltro
+        valor={"multi"}
+        nombre={"todos"}
+        handleClick={handleClick}
+        total={total}
+      />
+      <ItemFiltro
+        valor={"movie"}
+        nombre={"peliculas"}
+        handleClick={handleClick}
+      />
+      <ItemFiltro valor={"tv"} nombre={"series"} handleClick={handleClick} />
+      <ItemFiltro
+        valor={"person"}
+        nombre={"personas"}
+        handleClick={handleClick}
+      />
+    </StyledFiltros>
+  );
+};
+
+export default Filtros;
