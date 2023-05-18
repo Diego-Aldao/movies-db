8bdc69efffc9a9b9f6ee8019c822c303da9df166
Diego Aldao <diego_ind@live.com.ar>
2023-05-18 17:03:55 -0300

    añadido un archivo con las traducciones de los estados de series y peliculas, departamentos y tipos de series

---
+++ src/Utils/Traducciones.js
@@ -0,0 +1,44 @@
+export const estadosPeliculas = {
+  Canceled: "Cancelada",
+  "In Production": "En producción",
+  Planned: "Programada",
+  "Post Production": "Posproducción",
+  Released: "Estrenada",
+  Rumored: "Se rumorea",
+};
+
+export const estadosSeries = {
+  Canceled: "Cancelado",
+  Ended: "Finalizado",
+  "In Production": "En producción",
+  Pilot: "Piloto",
+  Planned: "Programado",
+  "Returning Series": "Volverá a emitirse",
+};
+
+export const tipoDeSerie = {
+  Documentary: "Documental",
+  Miniseries: "Miniseries",
+  News: "Noticias",
+  Reality: "Realidad",
+  Scripted: "Con guion",
+  "Talk Show": "Programa de entrevistas",
+  Video: "Vídeo",
+};
+
+export const departamentos = {
+  Acting: "Interpretación",
+  Actors: "Actores",
+  Art: "Arte",
+  Camera: "Cámara",
+  "Costume & Make-Up": "Vestuario y maquillaje",
+  Creator: "Creador",
+  Crew: "Equipo",
+  Directing: "Dirección",
+  Editing: "Edición",
+  Lighting: "Iluminación",
+  Production: "Producción",
+  Sound: "Sonido",
+  "Visual Effects": "Efectos visuales",
+  Writing: "Guion",
+};
