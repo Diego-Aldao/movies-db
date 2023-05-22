511b2af9ecb51a7b13f88e30a1d8e7401bb7081d
Diego Aldao <diego_ind@live.com.ar>
2023-05-22 18:03:55 -0300

    agregado un conditional rendering para mostrar los iconos de favorito y guardado, añadidos onClicks para navegar a la pagina detalle y para hacer el toggle del modal movil

--- src/components/Navbar/NavMobile.jsx
+++ src/components/Navbar/NavMobile.jsx
@@ -13,16 +13,44 @@ const StyledNavMobile = styled.nav`
   }
 `;
 
-const NavMobile = ({ handleClick }) => {
+const NavMobile = ({
+  handleClick,
+  hayFavoritos,
+  hayGuardados,
+  handleNavigation,
+  setModalIsVisible,
+}) => {
+  const handleVisible = () => {
+    setModalIsVisible((prevState) => !prevState);
+  };
+
   return (
     <StyledNavMobile>
-      <Icon icon="fluent:navigation-16-filled" className="hamburguesa" />
+      <Icon
+        icon="fluent:navigation-16-filled"
+        className="hamburguesa"
+        onClick={handleVisible}
+      />
       <Logo />
       <div className="iconos">
         <Icon icon="tabler:search" onClick={handleClick} className="search" />
-        <Icon icon="tabler:user" />
-        <Icon icon="tabler:heart" />
-        <Icon icon="tabler:bookmark" />
+        <Icon icon="tabler:user" onClick={handleNavigation} />
+        {hayFavoritos ? (
+          <span className="contador">
+            <span>{hayFavoritos}</span>
+            <Icon icon="tabler:heart-filled" />
+          </span>
+        ) : (
+          <Icon icon="tabler:heart" />
+        )}
+        {hayGuardados ? (
+          <span className="contador">
+            <span>{hayGuardados}</span>
+            <Icon icon="tabler:bookmark-filled" />
+          </span>
+        ) : (
+          <Icon icon="tabler:bookmark" />
+        )}
       </div>
     </StyledNavMobile>
   );
