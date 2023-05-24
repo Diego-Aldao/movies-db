fc4249f5aea84e2fb25ed7bb831ffb42c89f0557
Diego Aldao <diego_ind@live.com.ar>
2023-05-24 16:58:18 -0300

    reemplazados todos los componentes del contenido por dos componentes principales, uno para media y otro para persona

--- src/pages/PaginaDetalle.jsx
+++ src/pages/PaginaDetalle.jsx
@@ -1,21 +1,12 @@
 import { useParams } from "react-router-dom";
 import LayoutPrincipal from "../Layout/LayoutPrincipal";
-import Cast from "../components/PaginaDetalle/DetalleMedia/Cast";
-import Coleccion from "../components/PaginaDetalle/DetalleMedia/Coleccion";
-import Header from "../components/PaginaDetalle/DetalleMedia/Header/Header";
-import Main from "../components/PaginaDetalle/DetalleMedia/Main";
-import Media from "../components/PaginaDetalle/DetalleMedia/Media/Media";
-import SideInfo from "../components/PaginaDetalle/DetalleMedia/SideInfo/SideInfo";
-import Similares from "../components/PaginaDetalle/DetalleMedia/Similares";
 
 import useDetalle from "../hooks/useDetalle";
-import MainPersona from "../components/PaginaDetalle/DetallePersona/MainPersona";
-import SideInfoPersona from "../components/PaginaDetalle/DetallePersona/SideInfoPersona";
-import AboutPersona from "../components/PaginaDetalle/DetallePersona/AboutPersona";
-import ParticipacionesPersona from "../components/PaginaDetalle/DetallePersona/ParticipacionesPersona";
-import TablasPersona from "../components/PaginaDetalle/DetallePersona/Tablas/TablasPersona";
 import { useState, useEffect } from "react";
 import ScrollTop from "../components/ScrollTop";
+
+import DetalleMedia from "../components/PaginaDetalle/DetalleMedia/DetalleMedia";
+import DetallePersona from "../components/PaginaDetalle/DetallePersona/DetallePersona";
 
 const PaginaDetalle = () => {
   const params = useParams();
@@ -47,31 +38,9 @@ const PaginaDetalle = () => {
     <LayoutPrincipal>
       <ScrollTop />
       {dataMedia && tipo !== "person" ? (
-        <>
-          <Header data={{ ...dataMedia, media_type: tipo }} />
-          <Main>
-            <Cast cast={dataMedia?.credits} />
-            <Media idMedia={id} mediaType={tipo} />
-            {dataMedia.belongs_to_collection && (
-              <Coleccion coleccion={dataMedia?.belongs_to_collection} />
-            )}
-            <Similares dataSimilares={dataMedia?.similar} />
-            <SideInfo data={{ ...dataMedia, media_type: tipo }} />
-          </Main>
-        </>
+        <DetalleMedia dataMedia={dataMedia} tipo={tipo} id={id} />
       ) : (
-        dataPersona && (
-          <>
-            <MainPersona>
-              <SideInfoPersona data={dataPersona} />
-              <div className="main-info-persona">
-                <AboutPersona data={dataPersona} />
-                <ParticipacionesPersona data={dataPersona.combined_credits} />
-                <TablasPersona data={dataPersona.combined_credits} />
-              </div>
-            </MainPersona>
-          </>
-        )
+        dataPersona && <DetallePersona dataPersona={dataPersona} />
       )}
     </LayoutPrincipal>
   );
