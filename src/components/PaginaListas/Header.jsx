d03b9a1e33afe5c5485b82c3caa15f1af1943442
Diego Aldao <diego_ind@live.com.ar>
2023-05-25 11:12:39 -0300

    terminados los estilos y traido el titulo dentro del componente

--- src/components/PaginaListas/Header.jsx
+++ src/components/PaginaListas/Header.jsx
@@ -5,11 +5,17 @@ const StyledHeader = styled.header`
   h2 {
     text-transform: capitalize;
     font-size: clamp(24px, 3.5vw, 32px);
+    text-align: center;
   }
 `;
 
-const Header = ({ children }) => {
-  return <StyledHeader>{children}</StyledHeader>;
+const Header = ({ titulo, subCategoria }) => {
+  const newTitulo = titulo.replaceAll("-", " ");
+  return (
+    <StyledHeader>
+      <h2>{`${newTitulo} ${subCategoria}es`}</h2>
+    </StyledHeader>
+  );
 };
 
 export default Header;
